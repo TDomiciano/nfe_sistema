8fba02a554aebb4f1dc5233676a4075576144ceb
Theo <jeh.domiciano@gmail.com>
2026-05-26 15:44:03 -0300

    .

--- app.py
+++ app.py
@@ -61,31 +61,31 @@ def carregar_regras():
     try:
 
     tabela_icms = pd.read_excel(
-    "aliquotas.xlsx",
-    index_col=0
-)
+        "aliquotas.xlsx",
+        index_col=0
+    )
 
-# limpa colunas
-tabela_icms.columns = (
-    tabela_icms.columns
-    .astype(str)
-    .str.upper()
-    .str.strip()
-)
+    # limpa colunas
+    tabela_icms.columns = (
+        tabela_icms.columns
+        .astype(str)
+        .str.upper()
+        .str.strip()
+    )
 
-# limpa index
-tabela_icms.index = (
-    tabela_icms.index
-    .astype(str)
-    .str.upper()
-    .str.strip()
-)
+    # limpa index
+    tabela_icms.index = (
+        tabela_icms.index
+        .astype(str)
+        .str.upper()
+        .str.strip()
+    )
 
-# converte tudo para numero
-tabela_icms = tabela_icms.apply(
-    pd.to_numeric,
-    errors="coerce"
-)
+    # converte para numero
+    tabela_icms = tabela_icms.apply(
+        pd.to_numeric,
+        errors="coerce"
+    )
 
 except Exception as e:
 
