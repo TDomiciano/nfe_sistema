17f0c730814b2a8736a7c8f1285e1aacb4fab16b
Theo <jeh.domiciano@gmail.com>
2026-06-03 10:39:47 -0300

    a

--- app.py
+++ app.py
@@ -1152,10 +1152,7 @@ if arquivos:
                         file_name="auditoria_fiscal.xlsx",
                         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                     )
-
-    # =========================
-    # TABELA PRINCIPAL
-    # =========================
+    
     st.dataframe(
         df,
         use_container_width=True
